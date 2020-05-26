946df009e96b93a9c2a7478a846ec5ad6ce6ea39
Daniel Frederick <dannyfrederick360@gmail.com>
2020-05-26 15:03:50 -0400

    added support for abc site, formatting and article information simplification

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,16 @@
 from source_modules.fox import FoxQuery
+from source_modules.abc import ABCQuery
 
-temp = FoxQuery('trump')
-for i in temp.result_articles:
-    print(i.metatags['og:title'])
+query = 'trump'
+
+temp1 = FoxQuery(query)
+
+temp1 = FoxQuery(query)
+print('-----------------------------------------------------------------------------')
+print('First Article on Fox\'s site for query: "{}" -->\n{}'.format(query, temp1.result_articles[0].info))
+print('-----------------------------------------------------------------------------')
+
+temp2 = ABCQuery(query)
+print('-----------------------------------------------------------------------------')
+print('First Article on ABC\'s site for query: "{}" -->\n{}'.format(query, temp2.result_articles[0].info))
+print('-----------------------------------------------------------------------------')
--- backend/source_modules/abc.py
+++ backend/source_modules/abc.py
@@ -1,8 +1,8 @@
 '''
 Author: Daniel Frederick
-Date: May 21, 2020
+Date: May 26, 2020
 
-fox news site specific web scraper
+abc news site specific web scraper
 '''
 
 '''
@@ -14,32 +14,30 @@ from lxml import html
 import requests
 import json
 
-url = 'https://www.foxnews.com/'
-
     # exports standardized articles from query
     # query - string - search term
-class FoxQuery:
+class ABCQuery:
     def __init__(self, query):
         self.query = query
         # list of article objects
         self.result_articles = self.getResultArticles()
 
     def getResultArticles(self):
-        query_url_front = 'https://api.foxnews.com/search/web?q='
-        query_url_back = '+more:pagemap:metatags-pagetype:article+more:pagemap:metatags-dc.type:Text.Article&siteSearch=foxnews.com&siteSearchFilter=i&callback=__jp2'
+        query_url_front = 'https://abcnews.go.com/meta/api/search?q='
+        query_url_back = '&limit=10&sort=&type=Story&section=&totalrecords=true&offset=0'
         # --- get list of articles
         query_url = str(query_url_front) + str(self.query) + str(query_url_back)
         query_json = requests.get(query_url).content
         # format json so it can be parsed
-        query_json = query_json.decode('utf-8')[22:-3]
+        query_json = query_json.decode('utf-8')
         # parse json so it can be used as a dictionary
         parsed_query_json = json.loads(query_json)
         # get array of result article objects
-        results = parsed_query_json['items']
+        results = parsed_query_json['item']
         articles = []
         # loop through results and pick out articles only, then create an article instance and add it to an array
         for i in results:
-            metatags = i['pagemap']['metatags'][0]
+            metatags = i
             article = Article(i['link'], metatags)
             articles.append(article)
         return articles
@@ -51,21 +49,20 @@ class Article:
         # dictionary of metatags
         self.metatags = metatags
         self.url = url
-        self.title = metatags["dc.title"]
-        self.desc = metatags["dc.description"]
-        self.date = metatags["dc.date"]
-        self.image = metatags["og:image"]
-        # self.author = metatags["dc.creator"]
+        self.title = metatags["title"]
+        self.desc = metatags["description"]
+        self.date = metatags["pubDate"]
+        self.image = metatags["image"]
         self.article_content = self.getArticleContent()
 
-        self.info = 'Source : ABC News\nTitle: {}\nDescription: {}\nDate Published: {}\nURL: {}'.format(self.title,  self.desc, self.date, self.url)
+        self.info = 'Source : ABC News\nTitle: {}\nDescription: {}\nDate Published: {}\nURL: {}'.format(self.title, self.desc, self.date, self.url)
 
     # returns a string of article content
     def getArticleContent(self):
         article_page =  requests.get(self.url)
         article_tree = html.fromstring(article_page.content)
 
-        article_content_p_list = article_tree.xpath('//div[@class="article-content"]//p')
+        article_content_p_list = article_tree.xpath('//article[@class="Article__Content story"]//p')
         article_content = '' 
         for i in article_content_p_list:
             article_content = article_content + i.text_content()
